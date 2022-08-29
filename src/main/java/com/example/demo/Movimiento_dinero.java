6751b9879aebfe1dc1304985c95ea62d83ec6d62
luiscarlos27 <lszondas@gmail.com>
2022-08-29 12:18:19 -0500

    clase movimiento de dinero

---
+++ src/main/java/com/example/demo/Movimiento_dinero.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+
+public class Movimiento_dinero {
+    private String num_cuenta;
+    private double valor;
+    private LocalDate fecha;
+    private String banco;
+
+    public String getNum_cuenta() {
+        return num_cuenta;
+    }
+
+    public void setNum_cuenta(String num_cuenta) {
+        this.num_cuenta = num_cuenta;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+}
