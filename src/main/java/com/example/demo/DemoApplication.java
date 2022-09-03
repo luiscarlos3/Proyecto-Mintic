3a51e8aa3dd9c633e9854b57f758a67c26dac9c5
luiscarlos27 <lszondas@gmail.com>
2022-09-02 22:23:26 -0500

    actualizacion de los registos mas la conexion de ejemplo base de datos

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,27 @@
 package com.example.demo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
+
 @SpringBootApplication
+
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		//pruba();
-		//SpringApplication.run(DemoApplication.class, args);
-		Usuario exe = new Usuario();
-		exe.setApellido("silva");
-		System.out.println(exe.getApellido());
+		SpringApplication.run(DemoApplication.class, args);
+
+
+
+		//Usuario exe = new Usuario();
+		//exe.setApellido("silva");
+		//System.out.println(exe.getApellido());
 
 
 
--- src/main/java/com/example/demo/controllers/TaskController.java
+++ src/main/java/com/example/demo/controllers/TaskController.java
@@ -1,14 +1,12 @@
-package Controllers;
-
+package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class TaskController {
 
     @GetMapping("/tarea")
-    public String Tasklist(){
-        return "Esto es un recurso en http";
-    }
+    public String Hello(){return "<h1> esto es una prueba es un endpoint </h1>";}
 }
