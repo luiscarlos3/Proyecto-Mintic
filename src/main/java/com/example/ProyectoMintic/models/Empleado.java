7e83377ce37de1209285aad0f17874b72b0b9515
Santiago Enriquez <santiago9107@hotmail.com>
2022-09-05 11:16:14 -0600

    Update Empleado.java
    Modificación de clase empleado acorde a lo solicitado en descripción del proyecto.

--- src/main/java/com/example/ProyectoMintic/models/Empleado.java
+++ src/main/java/com/example/ProyectoMintic/models/Empleado.java
@@ -1,20 +1,10 @@
 package com.example.ProyectoMintic.models;
 
 public class Empleado {
-    private String identificacion;
     private String nombre;
-    private String apellido;
-    private String telefono;
     private String correo;
+    private String empresa;
     private String rol;
-
-    public String getIdentificacion() {
-        return identificacion;
-    }
-
-    public void setIdentificacion(String identificacion) {
-        this.identificacion = identificacion;
-    }
 
     public String getNombre() {
         return nombre;
@@ -24,28 +14,20 @@ public class Empleado {
         this.nombre = nombre;
     }
 
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
     public String getCorreo() {
         return correo;
     }
 
     public void setCorreo(String correo) {
         this.correo = correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
     }
 
     public String getRol() {
