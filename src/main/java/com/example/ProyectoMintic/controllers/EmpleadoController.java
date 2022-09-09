0d18dbd2f69c41f698f1dff5f076b6c216adf788
luiscarlos27 <lszondas@gmail.com>
2022-09-08 20:09:13 -0500

    se agrego los metodos get, delete, put

--- src/main/java/com/example/ProyectoMintic/controllers/EmpleadoController.java
+++ src/main/java/com/example/ProyectoMintic/controllers/EmpleadoController.java
@@ -1,28 +1,37 @@
 package com.example.ProyectoMintic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.ProyectoMintic.services.ServicesEmpleado;
 import com.example.ProyectoMintic.models.Empleado;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.*;
 
 @RestController
 @RequestMapping("personas")
 public class EmpleadoController {
     @Autowired
     private ServicesEmpleado empleadoServices;
+
     @PostMapping("/Guardar")
 
-    public  void guardar(@RequestBody Empleado persona){
+    public void guardar(@RequestBody Empleado persona) {
         empleadoServices.save(persona);
-
-
+    }
+    @GetMapping("/listar")
+    public List<Empleado>Listar(){
+        return empleadoServices.findAll();
+    }
+    @DeleteMapping("/eliminar/{identificacion}")
+    public void Eliminar(@PathVariable("identificacion") String identificacion){
+        empleadoServices.deleteById(identificacion);
+    }
+    @PutMapping("actualizar")
+    public void actualizar(@RequestBody Empleado empleado){
+        empleadoServices.save(empleado);
 
 
     }
-
-
 
 }
